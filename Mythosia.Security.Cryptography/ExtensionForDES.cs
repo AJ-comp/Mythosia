ea40dff79b935879777c425dc8dfb964114b46dd
AJ-comp <AhjLvFov@gmail.com>
2023-06-21 00:57:15 +0900

    Added the extension function for AES, and 3DES in Mythosia.Security.Cryptography project
    1. Added the extension function for AES in Mythosia.Security.Cryptography project. (support AES128, AES192, AES256)
    2. Added the extension function for 3DES in Mythosia.Security.Cryptography project.
    3. Added the manual for AES, and 3DES.

---
+++ Mythosia.Security.Cryptography/ExtensionForDES.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Mythosia.Security.Cryptography
+{
+    public static class ExtensionForDES
+    {
+        /***************************************************************************/
+        /// <summary>
+        /// Encrypts the provided data using 3DES (TripleDES) algorithm with the specified key and initialization vector (IV).
+        /// </summary>
+        /// <param name="toEncryptData">The data to encrypt.</param>
+        /// <param name="key">The key used for encryption.</param>
+        /// <param name="iv">The initialization vector (IV) used for encryption.</param>
+        /// <param name="tripleDES">The TripleDES object used for encryption.</param>
+        /// <returns>The encrypted data.</returns>
+        /***************************************************************************/
+        public static IEnumerable<byte> Encrypt3DES(this IEnumerable<byte> toEncryptData, IEnumerable<byte> key, IEnumerable<byte> iv, TripleDES tripleDES)
+        {
+            ICryptoTransform encryptor = tripleDES.CreateEncryptor(tripleDES.Key, tripleDES.IV);
+
+            var inputBytes = toEncryptData.ToArray();
+            return encryptor.TransformFinalBlock(inputBytes, 0, inputBytes.Length);
+        }
+
+
+        /***************************************************************************/
+        /// <summary>
+        /// Encrypts the specified data using TripleDES encryption algorithm with the given key, IV, cipher mode, and padding mode.
+        /// </summary>
+        /// <param name="toEncryptData">The data to encrypt.</param>
+        /// <param name="key">The key used for encryption.</param>
+        /// <param name="iv">The initialization vector used for encryption.</param>
+        /// <param name="cipherMode">The cipher mode used for encryption (default: CipherMode.CBC).</param>
+        /// <param name="paddingMode">The padding mode used for encryption (default: PaddingMode.PKCS7).</param>
+        /// <returns>The encrypted data.</returns>
+        /// /***************************************************************************/
+        public static IEnumerable<byte> Encrypt3DES(this IEnumerable<byte> toEncryptData, IEnumerable<byte> key, IEnumerable<byte> iv,
+                                                                            CipherMode cipherMode = CipherMode.CBC, PaddingMode paddingMode = PaddingMode.PKCS7)
+        {
+            using var tripleDES = TripleDES.Create();
+
+            tripleDES.Key = key.ToArray();
+            tripleDES.IV = iv.ToArray();
+            tripleDES.Mode = cipherMode;
+            tripleDES.Padding = paddingMode;
+
+            return Encrypt3DES(toEncryptData, key, iv, tripleDES);
+        }
+
+
+        /***************************************************************************/
+        /// <summary>
+        /// Encrypts the specified data using TripleDES encryption algorithm with the given key, IV, and padding mode.
+        /// The default cipher mode used is CipherMode.CBC.
+        /// </summary>
+        /// <param name="toEncryptData">The data to encrypt.</param>
+        /// <param name="key">The key used for encryption.</param>
+        /// <param name="iv">The initialization vector used for encryption.</param>
+        /// <param name="paddingMode">The padding mode used for encryption.</param>
+        /// <returns>The encrypted data.</returns>
+        /***************************************************************************/
+        public static IEnumerable<byte> Encrypt3DES(this IEnumerable<byte> toEncryptData, IEnumerable<byte> key, IEnumerable<byte> iv,
+                                                                            PaddingMode paddingMode)
+            => Encrypt3DES(toEncryptData, key, iv, CipherMode.CBC, paddingMode);
+
+
+
+        /***************************************************************************/
+        /// <summary>
+        /// Decrypts the provided encrypted data using 3DES (TripleDES) algorithm with the specified key and initialization vector (IV).
+        /// </summary>
+        /// <param name="encryptedData">The encrypted data to decrypt.</param>
+        /// <param name="key">The key used for decryption.</param>
+        /// <param name="iv">The initialization vector (IV) used for decryption.</param>
+        /// <param name="tripleDES">The TripleDES object used for decryption.</param>
+        /// <returns>The decrypted data.</returns>
+        /***************************************************************************/
+        public static IEnumerable<byte> Decrypt3DES(this IEnumerable<byte> encryptedData, IEnumerable<byte> key, IEnumerable<byte> iv, TripleDES tripleDES)
+        {
+            ICryptoTransform decryptor = tripleDES.CreateDecryptor(tripleDES.Key, tripleDES.IV);
+
+            var cipherText = encryptedData.ToArray();
+            var decryptedBytes = decryptor.TransformFinalBlock(cipherText, 0, cipherText.Length);
+            return decryptedBytes;
+        }
+
+
+        /***************************************************************************/
+        /// <summary>
+        /// Decrypts the specified encrypted data using TripleDES decryption algorithm with the given key, IV, cipher mode, and padding mode.
+        /// </summary>
+        /// <param name="encryptedData">The encrypted data to decrypt.</param>
+        /// <param name="key">The key used for decryption.</param>
+        /// <param name="iv">The initialization vector used for decryption.</param>
+        /// <param name="cipherMode">The cipher mode used for decryption (default: CipherMode.CBC).</param>
+        /// <param name="paddingMode">The padding mode used for decryption (default: PaddingMode.PKCS7).</param>
+        /// <returns>The decrypted data.</returns>
+        /***************************************************************************/
+        public static IEnumerable<byte> Decrypt3DES(this IEnumerable<byte> encryptedData, IEnumerable<byte> key, IEnumerable<byte> iv,
+                                                                            CipherMode cipherMode = CipherMode.CBC, PaddingMode paddingMode = PaddingMode.PKCS7)
+        {
+            using var tripleDES = TripleDES.Create();
+
+            tripleDES.Key = key.ToArray();
+            tripleDES.IV = iv.ToArray();
+            tripleDES.Mode = cipherMode;
+            tripleDES.Padding = paddingMode;
+
+            return Decrypt3DES(encryptedData, key, iv, tripleDES);
+        }
+
+
+
+        /***************************************************************************/
+        /// <summary>
+        /// Decrypts the specified encrypted data using TripleDES decryption algorithm with the given key, IV, and padding mode.
+        /// The default cipher mode used is CipherMode.CBC.
+        /// </summary>
+        /// <param name="encryptedData">The encrypted data to decrypt.</param>
+        /// <param name="key">The key used for decryption.</param>
+        /// <param name="iv">The initialization vector used for decryption.</param>
+        /// <param name="paddingMode">The padding mode used for decryption.</param>
+        /// <returns>The decrypted data.</returns>
+        /***************************************************************************/
+        public static IEnumerable<byte> Decrypt3DES(this IEnumerable<byte> encryptedData, IEnumerable<byte> key, IEnumerable<byte> iv,
+                                                                            PaddingMode paddingMode)
+            => Decrypt3DES(encryptedData, key, iv, CipherMode.CBC, paddingMode);
+    }
+}
---
+++ Mythosia.Security.Cryptography/KeyGenerator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Mythosia.Security.Cryptography
+{
+    public class KeyGenerator
+    {
+        private static IEnumerable<byte> GenerateKey(int keySize)
+        {
+            byte[] key = new byte[keySize / 8];
+
+            using var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(key);
+
+            return key;
+        }
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random key for AES-128 encryption.
+        /// </summary>
+        /// <returns>The generated AES-128 key.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> GenerateAES128Key() => GenerateKey(128);
+
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random key for AES-192 encryption.
+        /// </summary>
+        /// <returns>The generated AES-192 key.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> GenerateAES192Key() => GenerateKey(192);
+
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random key for AES-256 encryption.
+        /// </summary>
+        /// <returns>The generated AES-256 key.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> GenerateAES256Key() => GenerateKey(256);
+
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random IV (Initialization Vector) for AES-128 encryption.
+        /// </summary>
+        /// <returns>The generated AES-128 IV.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> GenerateAES128IV() => GenerateKey(128);
+
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random IV (Initialization Vector) for AES-192 encryption.
+        /// </summary>
+        /// <returns>The generated AES-192 IV.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> GenerateAES192IV() => GenerateKey(192);
+
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random IV (Initialization Vector) for AES-256 encryption.
+        /// </summary>
+        /// <returns>The generated AES-256 IV.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> GenerateAES256IV() => GenerateKey(256);
+
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random key for 3DES (TripleDES) encryption.
+        /// </summary>
+        /// <returns>The generated 3DES key.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> Generate3DESKey() => GenerateKey(192);
+
+
+        /******************************************************/
+        /// <summary>
+        /// Generates a random IV (Initialization Vector) for 3DES (TripleDES) encryption.
+        /// </summary>
+        /// <returns>The generated 3DES IV.</returns>
+        /******************************************************/
+        public static IEnumerable<byte> Generate3DESIV() => GenerateKey(64);
+    }
+}
